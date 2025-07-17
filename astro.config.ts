9d884f7dd331a905015255611810e61f46cae153
asdwrg <jun860262@gmail.com>
2025-07-17 21:07:19 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -15,7 +15,7 @@ import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: https://blog.ymdr.top,
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
