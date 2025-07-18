46288b0f8814ad6f07a9d38d2bc943def032ce01
asdwrg <jun860262@gmail.com>
2025-07-18 14:18:32 +0800

    upgarde

--- src/constants.ts
+++ src/constants.ts
@@ -2,13 +2,13 @@ import type { Props } from "astro";
 import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
+// import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+// import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import IconWeiXin from "@/assets/icons/IconWeiXin.svg";
-import IconQQ from "@/assets/icons/IconQQ.svg";
+// import IconQQ from "@/assets/icons/IconQQ.svg";
 import { SITE } from "@/config";
 import type { GiscusProps } from "@giscus/react";
 
