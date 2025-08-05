af967cd196045d54bf648342af6dd8752e3e5681
asdwrg <jun860262@gmail.com>
2025-08-05 23:56:46 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   site: 'https://blog.ymdr.top',
   integrations: [sitemap({
     filter: page => SITE.showArchives || !page.endsWith("/archives"),
-  }), react(), partytown({ config: { forward: ['dataLayer.push', 'gtag'] } })],
+  }), react(), partytown({ config: { debug: false, forward: ['dataLayer.push', 'gtag'] } })],
   markdown: {
     remarkPlugins: [
       remarkMath,
