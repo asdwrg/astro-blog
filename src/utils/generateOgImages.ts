e878498cd25358b96d7fb7be8fbb58be104fd174
asdwrg <jun860262@gmail.com>
2025-12-06 17:17:34 +0800

    Update generateOgImages.ts

--- src/utils/generateOgImages.ts
+++ src/utils/generateOgImages.ts
@@ -6,7 +6,7 @@ import siteOgImage from "./og-templates/site";
 function svgBufferToPngBuffer(svg: string) {
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
-  return pngData.asPng();
+  return Uint8Array.from(pngData.asPng());
 }
 
 export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
