a883dd34fa5f734647d37544ff779a1bec7e5690
asdwrg <jun860262@gmail.com>
2025-12-06 17:14:24 +0800

    Revert "Update og.png.ts"
    This reverts commit 4e967ad0925adbf77c255ff323043bce611fc6d6.

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,12 +1,7 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages";
 
-export const GET: APIRoute = async () => {
-  const ogImageBuffer = await generateOgImageForSite();
-  
-  const ogImageUint8Array = Uint8Array.from(ogImageBuffer); 
-
-  return new Response(ogImageUint8Array, {
+export const GET: APIRoute = async () =>
+  new Response(await generateOgImageForSite(), {
     headers: { "Content-Type": "image/png" },
   });
-};
