4e967ad0925adbf77c255ff323043bce611fc6d6
asdwrg <jun860262@gmail.com>
2025-12-06 17:03:44 +0800

    Update og.png.ts

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,12 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@/utils/generateOgImages";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
+export const GET: APIRoute = async () => {
+  const ogImageBuffer = await generateOgImageForSite();
+  
+  const ogImageUint8Array = Uint8Array.from(ogImageBuffer); 
+
+  return new Response(ogImageUint8Array, {
     headers: { "Content-Type": "image/png" },
   });
+};
